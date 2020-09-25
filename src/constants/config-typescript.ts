0332f422efa6e226d660e67d6314b20fbae09baa
vseriousv <gavrilovskirill@gmail.com>
2020-09-25 12:40:14 +0300

    Change api url

--- src/constants/config-typescript.ts
+++ src/constants/config-typescript.ts
@@ -1,6 +1,6 @@
 const config = {
-  // API_URL: "https://api.vtaward.ru",
-  API_URL: 'http://localhost:3000',
+  API_URL: "https://api.vtaward.ru",
+  // API_URL: 'http://localhost:3000',
   URL_AVATARS: "https://files.vtaward.ru/avatars/"
 };
 
