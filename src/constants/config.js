bf0f145304e3f5224dd5afe18daa562e840415d9
vseriousv <gavrilovskirill@gmail.com>
2021-11-09 23:42:39 +0300

    fix: change api url

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,6 @@
 const config = {
-	// API_URL: "http://api.vtaward.ru",
-	API_URL: "http://localhost:3000",
+	API_URL: "http://api.vtaward.ru",
+	// API_URL: "http://localhost:3000",
 	URL_AVATARS: "http://files.vtaward.ru/avatars/"
 };
 
